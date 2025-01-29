1864a61263a2f963ec88a878b645f06ef92f4c27
Diego Ramirez <diego.ramirez@sygno.mx>
2025-01-29 13:07:55 -0600

    Se agrega AlcaldiaWeb

---
+++ AlcaldiaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlcaldiaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
